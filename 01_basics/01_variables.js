d70833da98237e468b72f124e8bd82fd963cefc5
biplop32 <biploprai2003@gmail.com>
2025-05-12 15:11:04 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,37 @@
+const accountId = 144533
+let accountEmail = "biplop@gmail.com"
+var accountPassword = "12121"
+accontCity = "jhapa"
+
+
+// accountId =  122 // not allowed
+
+ accountEmail = "rai biplop@gmail.com"
+ accountPassword = "5555"
+ accountCity = "chaarali"
+let accountState;
+let accountAdress = "jhjhkh"
+
+
+console.log(accountId);
+
+/*
+prefer not use var
+becauseof issue in block scope and functional scope
+
+*/
+
+ console.table([accountId, accountEmail, accountPassword, accountCity, accountState, accountAdress])
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,18 @@
+
+
+// number => 2 to power 53
+// bigint 
+// string = ""
+// boolean => true/false
+// null =standalone value
+// undefined =
+// symbol => unique
+
+// object
+
+
+let name = "biplop"
+let age = 22
+let city = "dhulabari"
+
+console.log(typeof age)
