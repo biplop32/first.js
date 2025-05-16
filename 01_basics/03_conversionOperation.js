03b32d148855161a5318dc6d65db0ed591982db9
biplop32 <biploprai2003@gmail.com>
2025-05-16 16:12:06 +0000

    conversion of dataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+ let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+//  "33abc" => NaN
+//  True => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "biplop" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
